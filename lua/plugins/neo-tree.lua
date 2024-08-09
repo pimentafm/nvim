949b3f690804fdc0fd399b3353ce3bca3f8b975f
Fernando Pimenta <fernando.m.pimenta@gmail.com>
2024-08-08 21:26:33 -0300

    Show hidden files on tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -2,6 +2,14 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true,
+        show_hidden_count = true,
+        hide_dotfiles = false,
+        hide_gitignore = false,
+      },
+    },
     enable_diagnostics = true,
     default_component_configs = {
       git_status = {
