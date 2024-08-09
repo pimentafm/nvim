85a6fdb25ae336ed01eba666da8f2189bd12b46c
Fernando Pimenta <fernando.m.pimenta@gmail.com>
2024-08-08 22:25:39 -0300

    Update copilot configuration

--- lua/community.lua
+++ lua/community.lua
@@ -21,28 +21,5 @@ return {
       code_font_family = "JetBrainsMono Nerd Font",
       bg_color = "#a8b0d5",
     },
-  },
-  {
-    "jackMort/ChatGPT.nvim",
-    event = "VeryLazy",
-    config = function()
-      require("chatgpt").setup({
-        openai_params = {
-          model = "gpt-4o",
-          frequency_penalty = 0.0,
-          presence_penalty = 0,
-          max_tokens = 4095,
-          temperature = 0.2,
-          top_p = 0.1,
-          n = 1,
-        },
-      })
-    end,
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      "nvim-lua/plenary.nvim",
-      "folke/trouble.nvim",
-      "nvim-telescope/telescope.nvim"
-    }
   }
 }
